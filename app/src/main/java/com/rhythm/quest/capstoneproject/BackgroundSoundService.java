450f1ac10444641ecbad998a77efdf0c32cd4977
Elvin <Elvinmartinez308@gmail.com>
2018-12-13 15:45:01 -0500

    Test

--- app/src/main/java/com/rhythm/quest/capstoneproject/BackgroundSoundService.java
+++ app/src/main/java/com/rhythm/quest/capstoneproject/BackgroundSoundService.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.util.Log;
-
+//Sound
 
 public class BackgroundSoundService extends Service {
     private static final String TAG = null;
